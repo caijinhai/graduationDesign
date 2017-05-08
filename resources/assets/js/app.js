aa68499bfd0f2228cfe0273652da6966b7083c0b
caijinhai <911159490@qq.com>
2017-05-08 22:48:44 +0800

    hello laravel + vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,8 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+// Vue.component('example', require('./components/Example.vue'));
+Vue.components('hello', require('./components/Hello.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Graduation Design</title>
+</head>
+<body>
+    <h1 id="app"></h1>
+</body>
+</html>
